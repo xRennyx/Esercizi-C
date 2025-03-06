3a9582d87371f453981eec068823c4ea998f41d4
xRennyx <150335643+xRennyx@users.noreply.github.com>
2025-03-06 08:50:01 +0100

    Add files via upload

---
+++ waitPID()2.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    int dim = 5;
+    int v[dim];
+    int somma = 0;
+    float media;
+
+    int p2 = fork();
+    if (p2 == 0)  
+    {
+        
+        somma = 0;
+
+        
+        for (int i = 0; i < dim; i++)
+        {
+            printf("Inserisci numero: ");
+            scanf("%d", &v[i]);
+            somma = somma + v[i];
+        }
+
+        
+        printf("Vettore: ");
+        for (int i = 0; i < dim; i++)
+        {
+            printf("%d ", v[i]);
+        }
+        printf("\n");
+
+        int p3 = fork();
+        if (p3 == 0) 
+        {
+            for (int i = 0; i < dim; i++)
+            {
+                int minPos = i;
+                for (int j = i; j < dim; j++)
+                {
+                    if (v[minPos] > v[j])
+                    {
+                        minPos = j;
+                    }
+                }
+                int temp = v[minPos];
+                v[minPos] = v[i];
+                v[i] = temp;
+            }
+            printf("Valore minimo: %d\n", v[0]);
+            exit(0);  
+        }
+        int p4=fork();
+        if(p4==0)
+        {
+            for (int i = 0; i < dim; i++)
+            {
+                int maxPos = i;
+                for (int j = i; j < dim; j++)
+                {
+                    if (v[maxPos] < v[j])
+                    {
+                        maxPos = j;
+                    }
+                }
+                int temp = v[maxPos];
+                v[maxPos] = v[i];
+                v[i] = temp;
+            }
+            printf("Valore massimo: %d\n", v[0]);
+            exit(0);  
+        }
+        int p5=fork();
+        if(p5==0)
+        {
+            media= (float) somma/dim;
+            printf("Media: %.2f\n", media);
+        }
+
+        
+        wait(NULL);
+        exit(somma);  
+    }
+    else 
+    {
+        
+        int status;
+        waitpid(p2, &status, 0);
+
+        if (WIFEXITED(status))
+        {
+            somma = WEXITSTATUS(status);
+        }
+
+        printf("Somma: %d\n", somma);
+    }
+
+    return 0;
+}
