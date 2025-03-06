ac95142aee25ac1a5d012fbfef160722adf749a2
xRennyx <150335643+xRennyx@users.noreply.github.com>
2025-03-06 08:56:53 +0100

    Add files via upload

--- waitPID()2.c
+++ waitPID()2.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     int p2 = fork();
     if (p2 == 0)  
     {
-        
+        printf("Sono P2 PID= %d, mio padre ha PID=%d\n", getpid(), getppid());
         somma = 0;
 
         
@@ -36,6 +36,7 @@ int main(int argc, char *argv[])
         int p3 = fork();
         if (p3 == 0) 
         {
+            printf("Sono P3 PID= %d, mio padre ha PID=%d\n", getpid(), getppid());
             for (int i = 0; i < dim; i++)
             {
                 int minPos = i;
@@ -56,6 +57,7 @@ int main(int argc, char *argv[])
         int p4=fork();
         if(p4==0)
         {
+            printf("Sono P4 PID= %d, mio padre ha PID=%d\n", getpid(), getppid());
             for (int i = 0; i < dim; i++)
             {
                 int maxPos = i;
@@ -76,6 +78,7 @@ int main(int argc, char *argv[])
         int p5=fork();
         if(p5==0)
         {
+            printf("Sono P5 PID= %d, mio padre ha PID=%d\n", getpid(), getppid());
             media= (float) somma/dim;
             printf("Media: %.2f\n", media);
         }
